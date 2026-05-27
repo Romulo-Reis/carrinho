12863dedb815e719b46adfdad4c76992967f01e3
Romulo-Reis <romuloreistavares@hotmail.com>
2026-05-26 21:05:00 -0300

    feat: Cria o layout da tela home.

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,7 +1,45 @@
+import { BsCartPlus } from "react-icons/bs"
 export function Home() {
     return (
         <div>
-            <h1>Página HOME</h1>
+            <main className="w-full max-w-7xl px-4 mx-auto">
+                <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
+
+                <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
+                    <section className="w-full">
+                        <img
+                            className="w-full rounded-lg max-h-70 mb-2" 
+                            src="https://m.media-amazon.com/images/I/51lXl6nTXEL.jpg" 
+                            alt="Logo do produto" 
+                        />
+                        <p>Fone</p>
+                        <div className="flex gap-3 items-center">
+                            <strong className="text-zinc-700/90">
+                                R$ 10000
+                            </strong>
+                            <button className="bg-zinc-900 p-1 rounded">
+                                <BsCartPlus size={20} color="#FFF"/>
+                            </button>
+                        </div>
+                    </section>
+                    <section className="w-full">
+                        <img
+                            className="w-full rounded-lg max-h-70 mb-2" 
+                            src="https://m.media-amazon.com/images/I/51lXl6nTXEL.jpg" 
+                            alt="Logo do produto" 
+                        />
+                        <p>Fone</p>
+                        <div className="flex gap-3 items-center">
+                            <strong className="text-zinc-700/90">
+                                R$ 10000
+                            </strong>
+                            <button className="bg-zinc-900 p-1 rounded">
+                                <BsCartPlus size={20} color="#FFF"/>
+                            </button>
+                        </div>
+                    </section>
+                </div>
+            </main>
         </div>
     )
 }
